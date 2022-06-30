2d0811db0adbed30004bd05aba73719df1472311
heily-tech <90965441+heily-tech@users.noreply.github.com>
2022-06-30 12:02:57 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.modal_close');
-const enterBtn = document.querySelector('.modal_open_enter');
-const createBtn = document.querySelector('.modal_open_create');
-const enter = document.querySelector('.modal_enter');
-const create = document.querySelector('.modal_create');
+fetch("footer.html").then(response => {
+    return response.text()
+}).then(data => {
+    document.querySelector("footer").innerHTML = data;
+});
 
-enterBtn.onclick = function() {
-  modal.style.display = 'block';
-  create.style.display = 'none';
-  enter.style.display = 'block';
-}
-createBtn.onclick = function() {
-  modal.style.display = 'block';
-  enter.style.display = 'none';
-  create.style.display = 'block';
-}
-
-close.onclick = function() {
-        modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+fetch("main_header.html").then(response => {
+  return response.text()
+}).then(data => {
+  document.querySelector("header").innerHTML = data;
+});
